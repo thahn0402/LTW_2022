f8ded05943180d6718e48c5f75d37026263e5977
thahn0402 <91453590+thahn0402@users.noreply.github.com>
2022-11-30 15:41:37 +0700

    update db

---
+++ LTW_2022/src/main/java/context/DBContext.java
@@ -0,0 +1,30 @@
+package context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBContext {
+    private static String DB_URL = "jdbc:mysql://localhost:3306/ltw2022";
+    private static String USER_NAME = "root";
+    private static String PASSWORD = "";
+
+    public Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            System.out.println("connect successfully!");
+        } catch (Exception ex) {
+            System.out.println("connect failure!");
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(new DBContext().getConnection());
+        } catch (Exception e) {
+        }
+    }
+}
---
+++ LTW_2022/src/main/java/dao/DAO.java
@@ -0,0 +1,163 @@
+package dao;
+
+import entity.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    } Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct(){
+        List<Product> list = new ArrayList<>();
+        String query ="";
+        return list;
+    }
+
+}
